ece14a6d6b9689655f899e649881eb814d7b4e58
sreeharsha-rav <dev.sreeharsha@gmail.com>
2025-02-09 00:56:03 +0530

    fix: decouple and fix pino logger

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,17 +1,53 @@
-const pino = require("pino");
+const isDevelopment = process.env.NODE_ENV === "development";
 
-const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: "SYS:standard",
-      ignore: "pid,hostname",
+let logger;
+
+if (isDevelopment) {
+  // Development logger with pretty printing
+  const pino = require("pino");
+  logger = pino({
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "SYS:standard",
+        ignore: "pid,hostname",
+      },
     },
-  },
-  level: process.env.LOG_LEVEL || "info",
-});
-
-logger.info("Info message");
+    level: process.env.LOG_LEVEL || "info",
+  });
+} else {
+  // Production logger with basic console
+  logger = {
+    info: (obj, msg) => {
+      if (msg) {
+        console.log(JSON.stringify({ level: "info", msg, ...obj }));
+      } else {
+        console.log(JSON.stringify({ level: "info", msg: obj }));
+      }
+    },
+    error: (obj, msg) => {
+      if (msg) {
+        console.error(JSON.stringify({ level: "error", msg, ...obj }));
+      } else {
+        console.error(JSON.stringify({ level: "error", msg: obj }));
+      }
+    },
+    warn: (obj, msg) => {
+      if (msg) {
+        console.warn(JSON.stringify({ level: "warn", msg, ...obj }));
+      } else {
+        console.warn(JSON.stringify({ level: "warn", msg: obj }));
+      }
+    },
+    debug: (obj, msg) => {
+      if (msg) {
+        console.debug(JSON.stringify({ level: "debug", msg, ...obj }));
+      } else {
+        console.debug(JSON.stringify({ level: "debug", msg: obj }));
+      }
+    },
+  };
+}
 
 module.exports = logger;
